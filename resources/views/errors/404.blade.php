cfe9f88222d3ebdb4848e294ef3082a6decd630b
Pierre Tinard <pierre@tinard.fr>
2017-01-18 17:05:55 +0100

    remove dash 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,6 @@
 @section('navbar')
 <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
 @if (Auth::check())
-<li class="nav-item"><a href="/users/dashboard" class="nav-link">Dashboard</a></li>
 <li class="nav-item"><a href="/users/account" class="nav-link">My Account</a></li>
 <li class="nav-item"><a href=" /logout" class="nav-link">Logout <i>({{ Auth::user()->name }})</i></a></li>
 @else
