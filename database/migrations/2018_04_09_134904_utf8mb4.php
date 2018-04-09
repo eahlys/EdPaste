1b8a28987fdfd966b0ce2f6f024d6e8ea06bfdd8
Pierre Tinard <pierre@tinard.fr>
2018-04-09 15:54:17 +0200

    fix utf8mb4

---
+++ database/migrations/2018_04_09_134904_utf8mb4.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Utf8mb4 extends Migration
+{
+    /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up() {
+        DB::unprepared('ALTER TABLE `pastes` CONVERT TO CHARACTER SET utf8mb4');
+    }
+    
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down() {
+        DB::unprepared('ALTER TABLE `pastes` CONVERT TO CHARACTER SET utf8');
+    }
+}
